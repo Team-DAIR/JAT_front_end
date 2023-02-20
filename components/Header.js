2e1ab65c56abd9585deb20339af12c17c6229c42
Aubrey Corsetti <113921161+aubreycorsetti@users.noreply.github.com>
2023-02-20 13:16:34 -0800

    added necessary files and requirements.

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className='flex p-4 bg-rose-200 text-gray-800 font-medium text-2xl font-serif mb-32'>
+      <h1>Job Application Tracker</h1>
+    </header>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import Header from '../components/Header';
+import CreateForm from '../components/CreateForm';
+import ReportTable from '../components/ReportTable';
+import Footer from '../components/Footer.js';
+import LoginForm from '../components/LoginForm';
+import CookieStandAdmin from '../components/StandAdmin';
+import { useAuth } from '../contexts/auth';
+
+
+
+export default function Home() {
+
+  const { user, login } = useAuth();
+
+  return (
+    <>
+      <div
+        className="bg-scroll bg-no-repeat bg-cover"
+        style={{
+          backgroundImage: `url('https://images.unsplash.com/photo-1612198790700-0ff08cb726e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGNvb2tpZSUyMHN0YWNrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60')`,
+          height: "1000px",
+        }}
+      >
+        <Head>
+              <title>Cookie Stand Admin</title>
+              </Head>
+            {user ?
+                <CookieStandAdmin />
+                :
+                <LoginForm onLogin={login} />
+            }
+
+      </div>
+    </>
+  )
+}
