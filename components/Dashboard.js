bdf53a18ca449c501f23a35047f1c740cb084626
Aubrey Corsetti <113921161+aubreycorsetti@users.noreply.github.com>
2023-02-22 15:11:44 -0800

    added nav bar and edited formPage

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,18 +1,27 @@
-// HARDCODE HEADER!
-import Header from "./Header"
 import ApplicationForm from "./FormPage"
 import Navbar from "./Nav"
+import Footer from "./Footer"
 
 export default function Dashboard() {
   return (
     <>
-    
-    <Header>
-      <ApplicationForm />
-    </Header>
-      <Navbar/>
-    <h1> DASHBOARD GOES HERE </h1>
-    </>
 
+      <Navbar />
+      <header className='flex p-4 bg-black text-black font-mono mb-32'>
+        <header
+          className=" bg-no-repeat bg-cover"
+          style={{
+            backgroundImage: `url('https://images.unsplash.com/photo-1533134486753-c833f0ed4866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`,
+            height: "500px",
+            width: "100%",
+          }}
+        >
+
+          <ApplicationForm />
+        </header>
+      </header>
+        <h1> DASHBOARD GOES HERE </h1>
+        <Footer/>
+    </>
   )
 }
--- components/FormPage.js
+++ components/FormPage.js
@@ -20,16 +20,16 @@ export default function ApplicationForm() {
     }
 
     return (
-        <form className="flex w-3/4 p-6 mx-auto my-6 bg-rose-900/50 border border-rose-900 justify-center" onSubmit={handleSubmit}>
+        <form className="flex w-3/4 p-6 mx-auto my-6 bg-blue-900/50 border border-blue-900 justify-center" onSubmit={handleSubmit}>
             <fieldset className='w-11/12'>
-                <legend className='inline-grid content-center mx-4 my-2'>Add Location</legend>
-                <input className='content-center mx-2 my-2 w-6/12' placeholder='ex: Seatte' name='location' />
-                <button className='inline-grid text-center w-72 mx-2 my-2 px-6 py-4 bg-rose-300 text-gray-50'>Create Stand</button>
-                <legend className='inline-block p-3 '>Minimun Customers Per Hour</legend>
+                <legend className='inline-grid content-center text-white mx-4 my-2'>Position</legend>
+                <input className='content-center mx-2 my-2 w-6/12' placeholder='ex: Software Developer' name='location' />
+                <button className='text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-mono rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 m-4'>Add New Application</button>
+                <legend className=' text-white inline-block p-3 '>Minimun Customers Per Hour</legend>
                 <input className='relative left-0.5 mx-2 my-2 w-3/12' placeholder='minimum' name='minimum' defaultValue={0} />
-                <legend className='inline-grid content-center p-3'>Maximum Customers Per Hour</legend>
+                <legend className='inline-grid  text-white content-center p-3'>Maximum Customers Per Hour</legend>
                 <input className='content-center inline-block mx-2 my-2 w-3/12' placeholder='maximum' name='maximum' defaultValue={0} />
-                <legend className='inline-grid content-center p-3'>Average Cookies Per Sale</legend>
+                <legend className='inline-grid  text-white content-center p-3'>Average Cookies Per Sale</legend>
                 <input className='relative right-0.5 mx-2 my-2 w-3/12' placeholder='average' name='average' defaultValue={0} />
             </fieldset>
         </form>
