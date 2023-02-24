f3d6bc6f9baafb96bff2258048a80ac9208a733a
Aubrey Corsetti <113921161+aubreycorsetti@users.noreply.github.com>
2023-02-24 08:53:22 -0800

    added css to dashboard and about us

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -14,7 +14,7 @@ export default function Dashboard() {
       <div className='relative min-h-screen '>
 
         <NavBar />
-        <header className='flex p-4 bg-black text-black font-mono mb-32'>
+        <header className='flex p-4 bg-black text-black font-mono'>
 
           {/* <div className="bg-no-repeat bg-cover">
           <Image
@@ -29,19 +29,22 @@ export default function Dashboard() {
 
             style={{
               backgroundImage: `url('https://images.unsplash.com/photo-1533134486753-c833f0ed4866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')`,
-              height: "200px",
+              height: "300px",
               width: "100%",
             }}
           >
-            <h1 className='p-32 text-center text-white text-3xl font-mono tracking-wider'> DASHBOARD</h1>
+            <h1 className='pt-32 text-center text-white text-3xl font-mono tracking-wider'> DASHBOARD</h1>
+            <p className='pt-3 text-center text-white text-lg font-mono italic tracking-wider'> You're on the road to success! Keep it up!</p>
+
 
 
           </div>
         </header>
-        <div className='items-center flex flex-col align-center '>
+        <h1 className="text-white bg-black text-center text-2xl font-mono pt-11">Your Applications</h1>
+        <div className='items-center flex flex-col align-center bg-black px-28 py-11'>
           <JobList onDelete={deleteResource}/>
         </div>
-        <div className='mt-2 items-center flex flex-col align-center  '>
+        <div className='p-2 pb-32 items-center flex flex-col mx-auto bg-black'>
           <Doughnut
             className=''
             options={{ maintainAspectRatio: true }} />
