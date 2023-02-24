a17e7bc3632649f71956caa268dab62f7b098f13
ianfshirley <ianfshirley88@gmail.com>
2023-02-24 00:06:19 -0800

    added styling to accordion, deleted all the ugly jobs we made when testing

--- components/AccordionItem.js
+++ components/AccordionItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HiArrowCircleDown, HiX, HiTrash } from 'react-icons/hi';
 
-export default function AccordionItem ({ title, date_applied, notes, method_of_applications, referrals, first, second, third, rejected, offer, Id, Index, setIndex, onDelete }) {
+export default function AccordionItem({ title, date_applied, notes, method_of_applications, referrals, first, second, third, rejected, offer, Id, Index, setIndex, onDelete }) {
 
   const handleSetIndex = (Id) => Index !== Id && setIndex(Id);
 
@@ -16,82 +16,91 @@ export default function AccordionItem ({ title, date_applied, notes, method_of_a
         <div className="flex items-center justify-center pr-10">
           {
             Index != Id
-            ?
-            (<HiArrowCircleDown className="w-6 h-6 group-hover:text-white text-slate-800" />)
-            :
-            (
-            <div className="flex items-center">
-              <HiX className="w-6 h-6 group-hover:text-white text-slate-800" />
-              <HiTrash
-                className="w-6 h-6 group-hover:text-white text-slate-800"
-                onClick={() => onDelete(Id)}
-              />
-            </div>
-            )
+              ?
+              (<HiArrowCircleDown className="w-6 h-6 group-hover:text-white text-slate-800" />)
+              :
+              (
+                <div className="flex items-center">
+                  <HiX className="w-6 h-6 group-hover:text-white text-slate-800" />
+                  <HiTrash
+                    className="w-6 h-6 group-hover:text-white text-slate-800"
+                    onClick={() => onDelete(Id)}
+                  />
+                </div>
+              )
           }
         </div>
       </div>
       {Index === Id && (
-        <div>
-          <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Date Applied: {date_applied}
+        <div className=" w-2/3 bg-slate-100 rounded-md border-solid border-2 border-slate-500 mt-2">
+          <div className="flex justify-evenly text-center">
+            <div>
+              <div className="bg-slate-100 px-10 font-semibold text-slate-800  h-auto py-4 rounded-md">
+                Date Applied: {date_applied}
+              </div>
+              <div className="bg-slate-100 px-10 font-semibold text-slate-800  h-auto py-4 rounded-md">
+                Notes: {notes}
+              </div>
+            </div>
+            <div>
+              <div className="bg-slate-100 px-10 font-semibold text-slate-800 h-auto py-4 rounded-md">
+                Method of Application: {method_of_applications}
+              </div>
+              <div className="bg-slate-100 px-10 font-semibold text-slate-800 h-auto py-4 rounded-md">
+                Referral? {referrals}
+              </div>
+            </div>
           </div>
-          <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Notes: {notes}
+          <div className="flex flex-row text-center w-auto">
+            {
+              first
+                ?
+                <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
+                  First Interview: &#x2713;
+                </div>
+                :
+                ''
+            }
+            {
+              second
+                ?
+                <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
+                  Second Interview: &#x2713;
+                </div>
+                :
+                ''
+            }
+            {
+              third
+                ?
+                <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
+                  Third Interview: &#x2713;
+                </div>
+                :
+                ''
+            }
+            {
+              rejected
+                ?
+                <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
+                  Rejected: &#128546;
+                </div>
+                :
+                ''
+            }
+            {
+              offer
+                ?
+                <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
+                  Offer Received! &#129321;
+                </div>
+                :
+                ''
+            }
           </div>
-          <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Method of Application: {method_of_applications}
-          </div>
-          <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Referral? {referrals}
-          </div>
-          {
-            first
-            ? 
-            <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            First Interview: &#x2713;
-            </div>
-            :
-            ''
-          }
-          {
-            second
-            ? 
-            <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Second Interview: &#x2713;
-            </div>
-            :
-            ''
-          }
-          {
-            third
-            ? 
-            <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Third Interview: &#x2713;
-            </div>
-            :
-            ''
-          }
-          {
-            rejected
-            ? 
-            <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Rejected: &#128546;
-            </div>
-            :
-            ''
-          }
-          {
-            offer
-            ? 
-            <div className="bg-slate-100 px-10 font-semibold text-slate-800 w-3/4 h-auto rounded-md border-1 border-blue-300 mb-2 py-4">
-            Offer Received! &#129321;
-            </div>
-            :
-            ''
-          }
+
           {/* <button onClick={handleDelete}>Delete</button> */}
-          
+
         </div>
       )}
     </>
