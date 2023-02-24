6126b04b2e2e1f543c30475a891c40c371f8887b
Aubrey Corsetti <113921161+aubreycorsetti@users.noreply.github.com>
2023-02-24 10:09:49 -0800

    added css to add a job page

--- components/Doughnut.js
+++ components/Doughnut.js
@@ -8,7 +8,7 @@ const data = {
         'Indeed',
         'Company Site',
         'LinkedIn',
-    
+
     ],
     datasets: [{
         data: [300, 50, 100],
@@ -21,19 +21,19 @@ const data = {
             '#FF6384',
             '#36A2EB',
             '#FFCE56'
-        ]
+        ],
     }]
 };
 
 export default () => (
     <>
         <h1 className='text-white pb-11 font-mono text-2xl'>Your Application Stats</h1>
-    <div className='flex w-1/3 mt-2'>
-        <Doughnut
-            data={data}
-            max-width={100}
-            max-height={100}
-        />
-    </div>
+        <div className='flex w-1/3 mt-2'>
+            <Doughnut
+                data={data}
+                max-width={100}
+                max-height={100}
+            />
+        </div>
     </>
 );
