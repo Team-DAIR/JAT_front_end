67acc7fe4ce2d3b3ba5c4c57e284df21c27555f7
Aubrey Corsetti <113921161+aubreycorsetti@users.noreply.github.com>
2023-02-23 21:12:36 -0800

    fixed footer and about css

--- components/NavBar.js
+++ components/NavBar.js
@@ -24,7 +24,7 @@ export default function NavBar() {
       <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
 
         <Link
-          href='/dashboard'
+          href='/dash'
           className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
 
           Dashboard
